8312cfc9a29f18c0b4a4b368bc9969cc6c2e7e6a
unknown <kojima.hp8@jsdata.co.jp>
2023-12-12 15:03:37 +0900

    Image 修正

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -19,15 +19,13 @@ export default async function InvoicesTable({
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
           <div className="md:hidden">
             {invoices?.map((invoice) => (
-              <div
-                key={invoice.id}
-                className="mb-2 w-full rounded-md bg-white p-4"
-              >
+              <div key={invoice.id} className="mb-2 w-full rounded-md bg-white p-4">
                 <div className="flex items-center justify-between border-b pb-4">
                   <div>
                     <div className="mb-2 flex items-center">
                       <Image
                         src={invoice.image_url}
+                        alt="Invoice Image"
                         className="mr-2 rounded-full"
                         width={28}
                         height={28}
@@ -86,6 +84,7 @@ export default async function InvoicesTable({
                     <div className="flex items-center gap-3">
                       <Image
                         src={invoice.image_url}
+                        alt="Invoice Image"
                         className="rounded-full"
                         width={28}
                         height={28}
